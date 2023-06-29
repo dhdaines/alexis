62216dcc0d92f0447d55d18d88b53540251baa3e
David Huggins-Daines <dhd@ecolingui.ca>
2023-06-29 19:07:01 -0400

    fix: extraire les couleurs correctement et garder en-tete/pied avec O

--- alexi/pdf2csv.py
+++ alexi/pdf2csv.py
@@ -10,13 +10,12 @@ import logging
 import csv
 import re
 from pathlib import Path
-from typing import Optional
+from typing import Iterator, Any
 
 from tqdm import tqdm
 
 
 LOGGER = logging.getLogger("pdf2csv")
-RGB = re.compile(r"\((\d+(?:\.\d+)?),\s*(\d+(?:\.\d+)?),\s*(\d+(?:\.\d+)?)\)")
 
 
 def make_argparse():
@@ -24,15 +23,12 @@ def make_argparse():
     parser.add_argument("infile", help="Fichier PDF à traiter", type=Path)
     parser.add_argument("outfile", help="Fichier CSV à créer", type=Path)
     parser.add_argument(
-        "-m", "--detect-margins", help="Detecter les marges", action="store_true"
-    )
-    parser.add_argument(
         "-v", "--verbose", help="Émettre des messages", action="store_true"
     )
     return parser
 
 
-def remove_margins(page, words) -> tuple[int, Optional[int]]:
+def detect_margins(page, words) -> Iterator[dict[str,Any]]:
     if not words:
         return words
     margin_top = 0
@@ -50,7 +46,7 @@ def remove_margins(page, words) -> tuple[int, Optional[int]]:
             "page %d: numéro de page en pied trouvé à %f pt", page.page_number, letop
         )
         margin_bottom = letop
-    elif lesize < 9 and (page.height - lebottom) < 100:
+    elif lesize < 10 and (page.height - lebottom) < 48:
         LOGGER.info("page %d: pied de page trouvé à %f pt", page.page_number, letop)
         margin_bottom = letop
         # Il existe parfois deux lignes de pied de page
@@ -58,7 +54,7 @@ def remove_margins(page, words) -> tuple[int, Optional[int]]:
             if w["top"] == letop:
                 continue
             wsize = w["bottom"] - w["top"]
-            if wsize < 9 and letop - w["bottom"] < 9:
+            if wsize < 10 and letop - w["bottom"] < 10:
                 LOGGER.info(
                     "page %d: deuxième ligne de pied de page trouvé à %f pt",
                     page.page_number,
@@ -71,17 +67,16 @@ def remove_margins(page, words) -> tuple[int, Optional[int]]:
             "page %d: numéro de page en tête trouvé à %f", page.page_number, l1bottom
         )
         margin_top = l1bottom
-    elif l1size < 9 and l1top < 100:
+    elif l1size < 10 and l1top < 48:
         LOGGER.info("page %d: en-tête trouvé a %f pt", page.page_number, l1bottom)
         margin_top = l1bottom
-    return [
-        word
-        for word in words
-        if word["top"] >= margin_top and word["bottom"] <= margin_bottom
-    ]
+    for word in words:
+        if word["bottom"] <= margin_top or word["top"] >= margin_bottom:
+            word["tag"] = "O"
+        yield word
 
 
-def write_csv(pdf, path, margins=False):
+def write_csv(pdf: pdfplumber.PDF, path: Path):
     fields = []
     for p in pdf.pages:
         words = p.extract_words()
@@ -96,16 +91,22 @@ def write_csv(pdf, path, margins=False):
         writer.writeheader()
         for idx, p in enumerate(tqdm(pdf.pages)):
             words = p.extract_words()
-            if margins:
-                words = remove_margins(p, words)
+            words = detect_margins(p, words)
             # Index characters for lookup
             chars = dict(((c["x0"], c["top"]), c) for c in p.chars)
             LOGGER.info("traitement de la page %d", p.page_number)
             for w in words:
                 # Extract colour from first character (FIXME: assumes RGB space)
                 if c := chars.get((w["x0"], w["top"])):
-                    if rgb := RGB.match(str(c["stroking_color"])):
-                        w["r"], w["g"], w["b"] = rgb.groups()
+                    # OMG WTF pdfplumber!!!
+                    if isinstance(c["stroking_color"], list) or isinstance(c["stroking_color"], tuple):
+                        if len(c["stroking_color"]) == 1:
+                            w["r"] = w["g"] = w["b"] = c["stroking_color"][0]
+                        elif len(c["stroking_color"]) == 3:
+                            w["r"], w["g"], w["b"] = c["stroking_color"]
+                        else:
+                            LOGGER.warning("Espace couleur non pris en charge: %s",
+                                           c["stroking_color"])
                     else:
                         w["r"] = w["g"] = w["b"] = c["stroking_color"]
                 w["page"] = p.page_number
@@ -121,7 +122,7 @@ def main(args):
         tqdm = lambda x: x  # noqa: E731
     with pdfplumber.open(args.infile) as pdf:
         logging.info("processing %s", args.infile)
-        write_csv(pdf, args.outfile, args.detect_margins)
+        write_csv(pdf, args.outfile)
 
 
 if __name__ == "__main__":
