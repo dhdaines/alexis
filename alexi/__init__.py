0757695253a3734606cc2c40973f8b5053766b6d
David Huggins-Daines <dhd@ecolingui.ca>
2023-07-04 10:08:07 -0400

    feat: download et convert dans alexi cli

--- alexi/__init__.py
+++ alexi/__init__.py
@@ -5,7 +5,7 @@ Ce module est le point d'entrée principale pour le logiciel ALEXI.
 """
 
 import argparse
-
+from pathlib import Path
 
 from . import cli
 
@@ -17,21 +17,37 @@ def make_argparse() -> argparse.ArgumentParser:
     subp.add_parser(
         "download", help="Télécharger les documents plus récents du site web"
     ).set_defaults(func=cli.download.main)
+
     subp.add_parser(
         "select", help="Générer la liste de documents pour une ou plusieurs catégories"
     ).set_defaults(func=cli.select.main)
-    subp.add_parser(
+
+    convert = subp.add_parser(
         "convert", help="Convertir le texte et les objets des fichiers PDF en CSV"
-    ).set_defaults(func=cli.convert.main)
+    )
+    convert.add_argument(
+        "infile", help="Fichier PDF à traiter", type=Path
+    )
+    convert.add_argument(
+        "outfile", help="Fichier CSV à créer", type=Path
+    )
+    convert.add_argument(
+        "-v", "--verbose", help="Émettre des messages", action="store_true"
+    )
+    convert.set_defaults(func=cli.convert.main)
+    
     subp.add_parser(
         "segment", help="Extraire les unités de texte des CSV"
     ).set_defaults(func=cli.segment.main)
+    
     subp.add_parser(
         "extract", help="Extraire la structure des CSV segmentés en format JSON"
     ).set_defaults(func=cli.extract.main)
+    
     subp.add_parser(
         "index", help="Générer un index Whoosh sur les documents extraits"
     ).set_defaults(func=cli.index.main)
+    
     subp.add_parser("search", help="Effectuer une recherche sur l'index").set_defaults(
         func=cli.search.main
     )
--- alexi/cli/download.py
+++ alexi/cli/download.py
@@ -2,7 +2,18 @@
 Fonction de téléchargement d'ALEXI.
 """
 
+import subprocess
+
+
 def main(args):
     """Télécharger les fichiers avec wget"""
-    pass
+    subprocess.run(["wget",
+                    "--no-check-certificate",
+                    "--timestamping",
+                    "--recursive",
+                    "--level=1",
+                    "--accept-regex",
+                    r".*upload/documents/.*\.pdf",
+                    "https://ville.sainte-adele.qc.ca/publications.php"],
+                   check=True)
 
