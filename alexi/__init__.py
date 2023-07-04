dd63370b37dcbbcb4cbdaaa14fc0aaf31bff4c7f
David Huggins-Daines <dhd@ecolingui.ca>
2023-07-04 10:14:14 -0400

    refactor: reglements.py -> alexi select

--- alexi/__init__.py
+++ alexi/__init__.py
@@ -18,36 +18,46 @@ def make_argparse() -> argparse.ArgumentParser:
         "download", help="Télécharger les documents plus récents du site web"
     ).set_defaults(func=cli.download.main)
 
-    subp.add_parser(
+    select = subp.add_parser(
         "select", help="Générer la liste de documents pour une ou plusieurs catégories"
-    ).set_defaults(func=cli.select.main)
+    )
+    select.add_argument(
+        "-i",
+        "--infile",
+        help="Page HTML avec liste de publications",
+        type=Path,
+        default="ville.sainte-adele.qc.ca/publications.php",
+    )
+    select.add_argument(
+        "-s",
+        "--section",
+        help="Expression régulière pour sélectionner la section des documents",
+        default=r"règlements"
+    )
+    select.set_defaults(func=cli.select.main)
 
     convert = subp.add_parser(
         "convert", help="Convertir le texte et les objets des fichiers PDF en CSV"
     )
-    convert.add_argument(
-        "infile", help="Fichier PDF à traiter", type=Path
-    )
-    convert.add_argument(
-        "outfile", help="Fichier CSV à créer", type=Path
-    )
+    convert.add_argument("infile", help="Fichier PDF à traiter", type=Path)
+    convert.add_argument("outfile", help="Fichier CSV à créer", type=Path)
     convert.add_argument(
         "-v", "--verbose", help="Émettre des messages", action="store_true"
     )
     convert.set_defaults(func=cli.convert.main)
-    
+
     subp.add_parser(
         "segment", help="Extraire les unités de texte des CSV"
     ).set_defaults(func=cli.segment.main)
-    
+
     subp.add_parser(
         "extract", help="Extraire la structure des CSV segmentés en format JSON"
     ).set_defaults(func=cli.extract.main)
-    
+
     subp.add_parser(
         "index", help="Générer un index Whoosh sur les documents extraits"
     ).set_defaults(func=cli.index.main)
-    
+
     subp.add_parser("search", help="Effectuer une recherche sur l'index").set_defaults(
         func=cli.search.main
     )
--- alexi/cli/select.py
+++ alexi/cli/select.py
@@ -2,7 +2,19 @@
 Fonction de sélection de fichiers source d'ALEXI.
 """
 
+import re
+from pathlib import Path
+
+from bs4 import BeautifulSoup
+
+
 def main(args):
     """Trouver une liste de fichiers dans la page web des documents."""
-    pass
+    with open(args.infile) as infh:
+        soup = BeautifulSoup(infh, 'lxml')
+        for h2 in soup.find_all('h2', string=re.compile(args.section, re.I)):
+            ul = h2.find_next('ul')
+            for li in ul.find_all('li'):
+                path = Path(li.a["href"])
+                print(path.relative_to("/"))
 
