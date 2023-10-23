4a859c4dd6d3e8b7bb07ff8f088a9271edd25b61
David Huggins-Daines <dhd@ecolingui.ca>
2023-10-23 18:33:29 -0400

    feat: add script to get comparable bylaws

---
+++ comparables.sh
@@ -0,0 +1,7 @@
+wget --no-check-certificate --timestamping --recursive --level=1 https://www.val-morin.ca/reglementation_formulaires/reglementation_urbanisme.php
+wget --no-check-certificate --timestamping --recursive --level=1 https://www.valdavid.com/services-aux-citoyens/urbanisme-et-permis/reglements-durbanisme/
+wget --no-check-certificate --timestamping --recursive --level=1 https://www.piedmont.ca/fr/administration-municipale/reglements-municipaux/
+wget --no-check-certificate --timestamping --recursive --level=1 https://www.vss.ca/services-aux-citoyens/services/reglementation-durbanisme
+wget --no-check-certificate --timestamping --recursive --level=1 https://www.sadl.qc.ca/reglements-urbanisme/
+wget --no-check-certificate --timestamping --recursive --level=1 https://www.morinheights.com/Reglements
+wget --no-check-certificate --timestamping --recursive --level=1 https://lacmasson.com/services-aux-citoyens/plan-durbanisme
