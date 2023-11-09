3017115199b8780ee257c1d7310f290056df8578
David Huggins-Daines <dhd@ecolingui.ca>
2023-11-09 08:43:55 -0500

    fix: s/SeqTag/seqtag/

--- scripts/title_pages.py
+++ scripts/title_pages.py
@@ -21,14 +21,14 @@ def main():
         with open(p, "rt") as infh, open(args.outdir / p.name, "wt") as outfh:
             reader = csv.DictReader(infh)
             fieldnames = list(reader.fieldnames)
-            fieldnames.insert(0, "SeqTag")
+            fieldnames.insert(0, "seqtag")
             writer = csv.DictWriter(outfh, fieldnames=fieldnames)
             writer.writeheader()
             last_page = None
             for page, group in itertools.groupby(reader, lambda w: w["page"]):
                 contents = list(group)
                 for w in contents:
-                    w["SeqTag"] = "O"
+                    w["seqtag"] = "O"
                 if last_page is None:
                     writer.writerows(contents)
                 last_page = contents
