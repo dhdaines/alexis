f9775bcb0dbc5973610de5b3cca7f1e18d7d0983
David Huggins-Daines <dhd@ecolingui.ca>
2024-06-28 14:16:34 -0400

    refactor: reorganize 1

--- scripts/train_rnn.py
+++ scripts/train_rnn.py
@@ -32,33 +32,6 @@ def make_dataset(csvs):
         yield features, labels
 
 
-X, y = zip(*make_dataset(DATA))
-
-labelset = set(itertools.chain.from_iterable(y))
-id2label = sorted(labelset, reverse=True)
-label2id = dict((label, idx) for (idx, label) in enumerate(id2label))
-
-featnames = sorted(k for k in X[0][0] if not k.startswith("line:"))
-feat2id = {name: {"": 0} for name in featnames}
-for feats in itertools.chain.from_iterable(X):
-    for name, ids in feat2id.items():
-        if feats[name] not in ids:
-            ids[feats[name]] = len(ids)
-
-all_data = [
-    (
-        [[feat2id[name][feats[name]] for name in featnames] for feats in page],
-        [label2id[tag] for tag in labels],
-    )
-    for page, labels in zip(X, y)
-]
-
-train_data, dt_data = train_test_split(
-    all_data, train_size=0.8, shuffle=True, random_state=42
-)
-dev_data, test_data = train_test_split(dt_data, train_size=0.5)
-
-
 def pad_collate_fn(batch):
     sequences_tokens, sequences_labels, lengths = zip(
         *[
@@ -79,41 +52,54 @@ def pad_collate_fn(batch):
     return pack_padded_sequences_tokens, padded_sequences_labels
 
 
-cuda_device = 0
-device = torch.device("cuda:%d" % cuda_device if torch.cuda.is_available() else "cpu")
-batch_size = 32
-set_seeds(1234)
-
-train_loader = DataLoader(
-    train_data, batch_size=batch_size, shuffle=True, collate_fn=pad_collate_fn
-)
-dev_loader = DataLoader(dev_data, batch_size=batch_size, collate_fn=pad_collate_fn)
-test_loader = DataLoader(test_data, batch_size=batch_size, collate_fn=pad_collate_fn)
-
-featdims = {name: 32 if name in ("text", "lower") else 2 for name in featnames}
-num_layer = 1
-bidirectional = True
-dimension = sum(featdims.values())
+def pad_collate_fn_predict(batch):
+    # Require data to be externally sorted by length for prediction
+    # (otherwise we have no idea which output corresponds to which input! WTF Poutyne!)
+    sequences_tokens, lengths = zip(
+        *((torch.LongTensor(tokens), len(tokens)) for (tokens, labels) in batch)
+    )
+    lengths = torch.LongTensor(lengths)
+    padded_sequences_tokens = pad_sequence(
+        sequences_tokens, batch_first=True, padding_value=0
+    )
+    pack_padded_sequences_tokens = pack_padded_sequence(
+        padded_sequences_tokens, lengths.cpu(), batch_first=True
+    )
+    return pack_padded_sequences_tokens
 
 
 class MyNetwork(nn.Module):
-    def __init__(self):
+    def __init__(
+        self,
+        featnames,
+        featdims,
+        feat2id,
+        n_labels,
+        hidden_size=32,
+        num_layer=1,
+        bidirectional=True,
+    ):
         super().__init__()
         self.hidden_state = None
         self.embedding_layers = {}
         for name in featnames:
             self.embedding_layers[name] = nn.Embedding(
-                len(feat2id[name]), featdims[name], padding_idx=0, device=device
+                len(feat2id[name]),
+                featdims[name],
+                padding_idx=0,
             )
             self.add_module(f"embedding_{name}", self.embedding_layers[name])
+        dimension = sum(featdims.values())
         self.lstm_layer = nn.LSTM(
             input_size=dimension,
-            hidden_size=32,
+            hidden_size=hidden_size,
             num_layers=num_layer,
             bidirectional=bidirectional,
             batch_first=True,
         )
-        self.output_layer = nn.Linear(32 * (2 if bidirectional else 1), len(id2label))
+        self.output_layer = nn.Linear(
+            hidden_size * (2 if bidirectional else 1), n_labels
+        )
 
     def forward(self, pack_padded_tokens_vectors: PackedSequence | torch.Tensor):
         # https://discuss.pytorch.org/t/how-to-use-pack-sequence-if-we-are-going-to-use-word-embedding-and-bilstm/28184
@@ -140,12 +126,50 @@ class MyNetwork(nn.Module):
         tag_space = self.output_layer(lstm_out)
         tag_space = tag_space.transpose(
             -1, 1
-        )  # We need to transpose since it's a sequence
+        )  # We need to transpose since it's a sequence (but why?!)
         return tag_space
 
 
-my_network = MyNetwork()
+X, y = zip(*make_dataset(DATA))
 
+labelset = set(itertools.chain.from_iterable(y))
+id2label = sorted(labelset, reverse=True)
+label2id = dict((label, idx) for (idx, label) in enumerate(id2label))
+
+featnames = sorted(k for k in X[0][0] if not k.startswith("line:"))
+feat2id = {name: {"": 0} for name in featnames}
+for feats in itertools.chain.from_iterable(X):
+    for name, ids in feat2id.items():
+        if feats[name] not in ids:
+            ids[feats[name]] = len(ids)
+
+all_data = [
+    (
+        [[feat2id[name][feats[name]] for name in featnames] for feats in page],
+        [label2id[tag] for tag in labels],
+    )
+    for page, labels in zip(X, y)
+]
+
+train_data, dt_data = train_test_split(
+    all_data, train_size=0.8, shuffle=True, random_state=42
+)
+dev_data, test_data = train_test_split(dt_data, train_size=0.5)
+
+cuda_device = 0
+device = torch.device("cuda:%d" % cuda_device if torch.cuda.is_available() else "cpu")
+batch_size = 32
+set_seeds(1234)
+
+train_loader = DataLoader(
+    train_data, batch_size=batch_size, shuffle=True, collate_fn=pad_collate_fn
+)
+dev_loader = DataLoader(dev_data, batch_size=batch_size, collate_fn=pad_collate_fn)
+test_loader = DataLoader(test_data, batch_size=batch_size, collate_fn=pad_collate_fn)
+
+featdims = {name: 32 if name in ("text", "lower") else 2 for name in featnames}
+
+my_network = MyNetwork(featnames, featdims, feat2id, len(id2label))
 optimizer = optim.Adam(my_network.parameters(), lr=0.1)
 loss_function = nn.CrossEntropyLoss()
 model = Model(
@@ -161,29 +185,9 @@ model.fit_generator(
     epochs=200,
     callbacks=[
         ExponentialLR(gamma=0.99),
-        EarlyStopping(
-            monitor="val_fscore_macro", mode="max", patience=20, verbose=True
-        ),
+        EarlyStopping(monitor="val_acc", mode="max", patience=20, verbose=True),
     ],
 )
-
-
-def pad_collate_fn_predict(batch):
-    # Require data to be externally sorted by length for prediction
-    # (otherwise we have no idea which output corresponds to which input! WTF Poutyne!)
-    sequences_tokens, lengths = zip(
-        *((torch.LongTensor(tokens), len(tokens)) for (tokens, labels) in batch)
-    )
-    lengths = torch.LongTensor(lengths)
-    padded_sequences_tokens = pad_sequence(
-        sequences_tokens, batch_first=True, padding_value=0
-    )
-    pack_padded_sequences_tokens = pack_padded_sequence(
-        padded_sequences_tokens, lengths.cpu(), batch_first=True
-    )
-    return pack_padded_sequences_tokens
-
-
 ordering, sorted_test_data = zip(
     *sorted(enumerate(test_data), reverse=True, key=lambda x: len(x[1][0]))
 )
@@ -194,9 +198,7 @@ out = model.predict_generator(test_loader, concatenate_returns=False)
 predictions = []
 lengths = [len(tokens) for tokens, _ in sorted_test_data]
 for batch in out:
-    # fucking torch transpose doesn't fucking work like fucking numpy
-    # transpose, what the fuck also why the fuck do we have to even do
-    # this, what the fucking fuck pytorch
+    # numpy.transpose != torch.transpose because Reasons
     batch = batch.transpose((0, 2, 1)).argmax(-1)
     for length, row in zip(lengths, batch):
         predictions.append(row[:length])
