6ae88b1d8d9782b53f7d44b6a796969a0e5c6ccc
David Huggins-Daines <dhd@ecolingui.ca>
2024-07-16 12:32:59 -0400

    fix: use 1

--- results/run_rnn.sh
+++ results/run_rnn.sh
@@ -2,5 +2,5 @@
 
 python scripts/train_rnn.py -x 4 --early-stopping --nepoch 100 -s results/rnnscores.csv data/*.csv
 python scripts/test_rnn.py --model alexi/models/rnn.pt data/patches/*.csv
-python scripts/train_rnn_crf.py --freeze -x 4 --nepoch 1 -i rnn.pt -s results/rnncrffreezealltrans.csv data/*.csv
+python scripts/train_rnn_crf.py --freeze -x 4 --nepoch 1 -i rnn.pt -s results/rnncrffreezealltrans1.csv data/*.csv
 python scripts/test_rnn.py --model alexi/models/rnn_crf.pt data/patches/*.csv
