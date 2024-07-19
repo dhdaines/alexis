f090aeb5926b4f310589e5260442301502fb996f
David Huggins-Daines <dhd@ecolingui.ca>
2024-07-19 12:49:33 -0400

    feat: evaluate CRF on patches too (surprising results)

--- results/run.sh
+++ results/run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 for features in text+ text+layout text+layout+structure; do
-    python scripts/train_crf.py --features $features --labels bonly -x 4 data/*.csv -s results/$features-x4.csv
+    python scripts/train_crf.py --features $features --labels bonly \
+           -x 4 data/*.csv -s results/$features-x4.csv -o results/cnn_$features
+    python scripts/test_crf_voting.py -m results/cnn_$features data/patches/*.csv \
+           > results/$features-patches.txt
 done
