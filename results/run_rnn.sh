3eaf4f8663f4d8cca56f68d0e623b7c5584b9202
David Huggins-Daines <dhd@ecolingui.ca>
2024-07-19 09:04:57 -0400

    feat: store results

--- results/run_rnn.sh
+++ results/run_rnn.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python scripts/train_rnn.py -x 4 --early-stopping --nepoch 100 -s results/rnnscores.csv data/*.csv
-python scripts/test_rnn.py --model alexi/models/rnn.pt data/patches/*.csv
-python scripts/train_rnn_crf.py --freeze -x 4 --nepoch 1 -i rnn.pt -s results/rnncrffreezealltrans1.csv data/*.csv
-python scripts/test_rnn.py --model alexi/models/rnn_crf.pt data/patches/*.csv
+python scripts/train_rnn.py -x 4 --early-stopping --nepoch 100  -s results/rnnscores.csv data/*.csv
+python scripts/test_rnn_voting.py --model rnn.pt  data/patches/*.csv | tee results/rnnpatches.txt
+python scripts/train_rnn_crf.py --freeze -x 4 --nepoch 1 -i rnn.pt  -s results/rnncrffreezealltrans1.csv data/*.csv
+python scripts/test_rnn_voting.py --model rnn_crf.pt  data/patches/*.csv | tee results/rnncrfpatches.txt
