826b291889e8d1dbcc1b6ff198886e9d27e62ac2
David Huggins-Daines <dhd@ecolingui.ca>
2024-07-24 22:54:04 -0400

    fix: supprimer des scripts inutilises

--- scripts/comparables.sh
+++ scripts/comparables.sh
@@ -1,7 +1,8 @@
-wget --no-check-certificate --timestamping --recursive --level=1 https://www.val-morin.ca/reglementation_formulaires/reglementation_urbanisme.php
-wget --no-check-certificate --timestamping --recursive --level=1 https://www.valdavid.com/services-aux-citoyens/urbanisme-et-permis/reglements-durbanisme/
-wget --no-check-certificate --timestamping --recursive --level=1 https://www.piedmont.ca/fr/administration-municipale/reglements-municipaux/
-wget --no-check-certificate --timestamping --recursive --level=1 https://www.vss.ca/services-aux-citoyens/services/reglementation-durbanisme
-wget --no-check-certificate --timestamping --recursive --level=1 https://www.sadl.qc.ca/reglements-urbanisme/
-wget --no-check-certificate --timestamping --recursive --level=1 https://www.morinheights.com/Reglements
-wget --no-check-certificate --timestamping --recursive --level=1 https://lacmasson.com/services-aux-citoyens/plan-durbanisme
+wget --accept pdf --no-check-certificate --timestamping --recursive --level=1 https://vsadm.ca/citoyens/reglementation/reglementation-durbanisme/
+wget --accept pdf --no-check-certificate --timestamping --recursive --level=1 https://www.val-morin.ca/reglementation_formulaires/reglementation_urbanisme.php
+wget --accept pdf --no-check-certificate --timestamping --recursive --level=1 https://www.valdavid.com/services-aux-citoyens/urbanisme-et-permis/reglements-durbanisme/
+wget --accept pdf --no-check-certificate --timestamping --recursive --level=1 https://www.piedmont.ca/fr/administration-municipale/reglements-municipaux/
+wget --accept pdf --no-check-certificate --timestamping --recursive --level=1 https://www.vss.ca/services-aux-citoyens/services/reglementation-durbanisme
+wget --accept pdf --no-check-certificate --timestamping --recursive --level=1 https://www.sadl.qc.ca/reglements-urbanisme/
+wget --accept pdf --no-check-certificate --timestamping --recursive --level=1 https://www.morinheights.com/Reglements
+wget --accept pdf --no-check-certificate --timestamping --recursive --level=1 https://lacmasson.com/services-aux-citoyens/plan-durbanisme
