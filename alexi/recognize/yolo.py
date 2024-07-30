3d6db975d9e718cf5b049d94fb63a4cd0d1407b9
David Huggins-Daines <dhd@ecolingui.ca>
2024-07-30 17:30:14 -0400

    refactor: remove unused yolo

--- alexi/recognize/yolo.py
+++ alexi/recognize/yolo.py
@@ -1,7 +1,4 @@
-import argparse
-import csv
 import logging
-import re
 from os import PathLike
 from pathlib import Path
 from typing import Iterable, Iterator, Union
@@ -9,12 +6,9 @@ from typing import Iterable, Iterator, Union
 import numpy as np
 import pdfplumber
 from huggingface_hub import hf_hub_download  # type: ignore
-from pdfplumber.utils.geometry import obj_to_bbox
 from ultralytics import YOLO  # type: ignore
 
-from alexi import segment
 from alexi.analyse import Bloc
-from alexi.convert import FIELDNAMES
 from alexi.recognize import Objets
 
 LOGGER = logging.getLogger(Path(__file__).stem)
@@ -60,6 +54,7 @@ class ObjetsYOLO(Objets):
             image = page.to_image(
                 resolution=scale_to_model(page, 640), antialias=True
             ).original
+            # FIXME: could do minibatches (not sure it helps)
             results = self.model(
                 source=image,
                 # show_labels=True,
@@ -98,124 +93,3 @@ class ObjetsYOLO(Objets):
                         _page_number=page_number,
                         _bbox=tuple(box.round()),
                     )
-
-
-def main():
-    parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument("pdf_or_png", type=Path)
-    parser.add_argument("csv", type=argparse.FileType("rt"))
-    parser.add_argument("out", type=argparse.FileType("wt"))
-    args = parser.parse_args()
-
-    yolo_model = hf_hub_download(
-        repo_id="DILHTWD/documentlayoutsegmentation_YOLOv8_ondoclaynet",
-        filename="yolov8x-doclaynet-epoch64-imgsz640-initiallr1e-4-finallr1e-5.pt",
-    )
-    docseg_model = YOLO(yolo_model)
-
-    if args.pdf_or_png.exists():
-        pdf = pdfplumber.open(args.pdf_or_png)
-        images = (
-            page.to_image(resolution=scale_to_model(page, 640), antialias=True).original
-            for page in pdf.pages
-        )
-    else:
-        pngdir = args.pdf_or_png.parent
-        pngre = re.compile(re.escape(args.pdf_or_png.name) + r"-(\d+)\.png")
-        pngs = []
-        for path in pngdir.iterdir():
-            m = pngre.match(path.name)
-            if m is None:
-                continue
-            pngs.append((int(m.group(1)), path))
-        images = (path for _idx, path in sorted(pngs))
-
-    reader = csv.DictReader(args.csv)
-    fieldnames = FIELDNAMES[:]
-    fieldnames.insert(0, "yolo")
-    writer = csv.DictWriter(args.out, fieldnames, extrasaction="ignore")
-    writer.writeheader()
-    for image, words in zip(images, segment.split_pages(reader)):
-        results = docseg_model(
-            source=image,
-            show_labels=True,
-            show_boxes=True,
-            show_conf=True,
-        )
-        assert len(results) == 1
-        entry = results[0]
-
-        # Probably should do some kind of spatial indexing
-        def boxsort(e):
-            """Sort by topmost-leftmost-tallest-widest."""
-            _, b = e
-            return (b[1], b[0], -(b[3] - b[1]), -(b[2] - b[0]))
-
-        if len(entry.boxes.xyxy) == 0:
-            continue
-        ordering, boxes = zip(
-            *sorted(
-                enumerate(bbox.cpu().numpy() for bbox in entry.boxes.xyxy),
-                key=boxsort,
-            )
-        )
-        labels = [entry.names[entry.boxes.cls[idx].item()] for idx in ordering]
-        page_width, page_height = float(words[0]["page_width"]), float(
-            words[0]["page_height"]
-        )
-        img_height, img_width = entry.orig_shape
-        print("scale x", page_width / img_width)
-        print("scale y", page_height / img_height)
-        boxes = np.array(boxes)
-        boxes[:, [0, 2]] = boxes[:, [0, 2]] * page_width / img_width
-        boxes[:, [1, 3]] = boxes[:, [1, 3]] * page_height / img_height
-        for label, box in zip(labels, boxes):
-            print(label, box)
-
-        # Boxes are (luckily) in the same coordinate system as
-        # pdfplumber. But... YOLO leaves off little bits of text
-        # particularly at the right edge, so we can't rely on
-        # containment to match them to words
-        def totally_contained(boxes, bbox):
-            return (
-                (bbox[[0, 1]] >= boxes[:, [0, 1]]) & (bbox[[2, 3]] <= boxes[:, [2, 3]])
-            ).all(1)
-
-        def mostly_contained(boxes, bbox):
-            """Calculate inverse ratio of bbox to its intersection with each box."""
-            # FIXME: This assumes boxes are normalized...
-            # print("box", bbox)
-            # print("boxes", boxes)
-            top_left = np.maximum(bbox[[0, 1]], boxes[:, [0, 1]])
-            bottom_right = np.minimum(bbox[[2, 3]], boxes[:, [2, 3]])
-            intersection = np.hstack((top_left, bottom_right))
-            # print("intersections", intersection)
-            area = (bbox[2] - bbox[0]) * (bbox[3] - bbox[1])
-            assert area >= 0
-            width = np.maximum((intersection[:, 2] - intersection[:, 0]), 0)
-            height = np.maximum((intersection[:, 3] - intersection[:, 1]), 0)
-            # print("width", width)
-            # print("height", height)
-            iarea = width * height
-            # print("area", area)
-            # print("iarea", iarea)
-            return iarea / area
-
-        prev_label = None
-        for w in words:
-            bbox = np.array([float(w) for w in obj_to_bbox(w)])
-            ratio = mostly_contained(boxes, bbox)
-            # print("ratio", ratio)
-            in_box = ratio.argmax()
-            in_ratio = ratio.max()
-            # print("in_box", in_box, "in_ratio", in_ratio)
-            # in_labels = [labels[idx] for idx, val in enumerate(ratio) if val > 0.5]
-            label = in_box if in_ratio > 0.5 else None
-            iob = "B" if label != prev_label else "I"
-            prev_label = label
-            w["yolo"] = f"{iob}-{labels[label]}" if label is not None else "O"
-            writer.writerow(w)
-
-
-if __name__ == "__main__":
-    main()
