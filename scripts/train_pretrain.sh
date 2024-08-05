a642f5dee955bb76a1910988e5cf75b383059ab4
David Huggins-Daines <dhd@ecolingui.ca>
2024-08-05 10:15:10 -0400

    fix: no, whoops, that was cheating

--- scripts/train_pretrain.sh
+++ scripts/train_pretrain.sh
@@ -1,23 +1,19 @@
 #!/bin/sh
 
 set -e
-hs=160
-mf=5
+: ${hs:=160}
+: ${mf:=5}
 
-# Extract features
-python scripts/train_rnn.py -o feats.pt --hidden-size $hs \
-       --nepoch 1 --min-feat $mf data/*.csv
-
-# Pre-train on lq+data
+# Pre-train on lq
 python scripts/train_rnn.py -o lqfeats.pt --hidden-size $hs --nepoch 5 -i feats.pt \
-       data/*.csv legisquebec/train/*.csv
+       legisquebec/train/*.csv
 
 # Fine-tune on data only
 python scripts/train_rnn.py -i lqfeats.pt -x 4 --hidden-size $hs \
        --early-stopping --nepoch 100 \
-       -s rnn-pretrain-feats-$hs.csv data/*.csv
+       -s rnn-pretrain-feats-$hs-$mf.csv data/*.csv
 
 # Fine-tune again to validate
 python scripts/train_rnn.py -i lqfeats.pt -x 4 --hidden-size $hs \
        --early-stopping --nepoch 100 \
-       -s rnn-pretrain-feats-$hs-rerun.csv data/*.csv
+       -s rnn-pretrain-feats-$hs-$mf-rerun.csv data/*.csv
