f22c26142ef39dfa20f6d59952b263893c89490e
David Huggins-Daines <dhd@ecolingui.ca>
2025-02-06 15:53:11 -0500

    fix(ci): update download script

--- scripts/download.sh
+++ scripts/download.sh
@@ -3,8 +3,12 @@
 set -e
 
 alexi -v download --exclude=/derogation \
-         --exclude='\d-[aA]dopt' --exclude='-Z-\d' \
-         --exclude='-[rR]eso'
+      --exclude='\d-[aA]dopt' \
+      --exclude='-Z-\d' \
+      --exclude='-\d-redevances' \
+      --exclude='-[rR]eso' \
+      --exclude=Plan-de-zonage \
+      --exclude=AnnexeZonage
 for d in download/*.pdf; do
     bn=$(basename $d .pdf)
     for dd in data/train data/dev data/test; do
@@ -13,8 +17,8 @@ for d in download/*.pdf; do
         fi
     done
 done
-alexi -v download -u https://vsadm.ca/citoyens/reglementation/reglementation-durbanisme/ -o download/vsadm --all-pdf-links
+alexi -v download -u https://vsadm.ca/citoyens/reglementation/reglementation-durbanisme/ -o download/vsadm --all-links
 alexi -v download -u https://www.vss.ca/services-aux-citoyens/services/reglementation-durbanisme/ \
-      -o download/vss --all-pdf-links -x '[Aa]nnexe'
+      -o download/vss --all-links -x '[Aa]nnexe'
 alexi -v download -u https://www.ville.prevost.qc.ca/guichet-citoyen/services/urbanisme/ \
-      -o download/prevost --all-pdf-links -x Annexe -x Formulaires -x PUMD -x PMAD
+      -o download/prevost --all-links -x Annexe -x Formulaires -x PUMD -x PMAD
